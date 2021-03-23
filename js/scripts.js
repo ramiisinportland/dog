674cfa0ad904bc94c4b390f93676276186f21ef1
Rami <ramiahmedportland@gmail.com>
2021-03-23 16:11:41 -0700

    changed the color for the background

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("rosybrown");
+  });
+
+$("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+});
+  
+
+
+
+
+
+
+
+
+  
+  
+  // $(document).ready(function() {
+  //   $("button#themeOriginal").click(function() {
+  //     $("body").removeClass();
+  //     $("body").addClass("green-background");
+  //   });
